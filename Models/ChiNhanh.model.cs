3c067333287bb57f246aeb7c66bd58eb81de0362
lepro83 <72549881+lepro83@users.noreply.github.com>
2022-11-03 10:47:35 +0700

    CCCCCCCC

---
+++ Models/ChiNhanh.model.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace QuanLyQuanGaRan.Models
+{
+    public class ChiNhanh
+    {
+        private string maCN;
+        private string tenCN;
+        private string diaChi;
+
+        [Required(ErrorMessage = "Thieu ma chi nhanh!!")]
+        public string MaCN
+        {
+            get { return maCN; }
+            set { maCN = value; }
+        }
+
+        public string TenCN
+        {
+            get { return tenCN; }
+            set { tenCN = value; }
+        }
+
+        public string DiaChi
+        {
+            get { return diaChi; }
+            set { diaChi = value; }
+        }
+
+    }
+}
---
+++ Models/NhanVien.model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanGaRan.Models
+{
+    public class NhanVien
+    {
+        private string maNV;
+        private string tenNV;
+        private string gioitinh;
+        private DateTime ngaysinh;
+
+
+    }
+}
